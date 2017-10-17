c20d97e628aba46820538090a73e153ebb19c1a5
AndreiStelea <stelea.andrei@gmail.com>
2017-10-17 18:48:27 +0300

    Start work at dao operations

--- src/ro/astl/userservice/dao/DaoLayerImpl.java
+++ src/ro/astl/userservice/dao/DaoLayerImpl.java
@@ -1,9 +1,22 @@
 package ro.astl.userservice.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import ro.astl.userservice.model.User;
 
 public class DaoLayerImpl implements DaoLayer {
+	
+	private static final String DB_URL = "jdbc:mysql://localhost:3306";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "wltmngr";
+	
 	private static DaoLayer instance = DaoLayerImpl.getInstance();
+	private DataSource dataSource;
+	
 
 	private DaoLayerImpl(){	
 	}
@@ -16,11 +29,22 @@ public class DaoLayerImpl implements DaoLayer {
 
 	@Override
 	public User getUserbyUsername(String Username) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = new User();
+		try(Connection conn = DaoLayerImpl.getConnection();
+			PreparedStatement stmnt = DaoLayerImpl.prepareStatement("getUserbyUsername", conn, Username);
+			ResultSet rs = stmnt.executeQuery()){
+			while(rs.next()) {
+				user.setId(rs.getInt("id"));
+				user.setUsername(rs.getString("username"));
+				user.setPassword(rs.getString("password"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return user;
 	}
 	
-	private static final DaoLayer getInstance(){
+	public static final DaoLayer getInstance(){
 		if(instance == null){
 			synchronized(DaoLayerImpl.class){
 				if(instance == null){
@@ -29,6 +53,38 @@ public class DaoLayerImpl implements DaoLayer {
 			}
 		}
 		return instance;
+	}
+	
+	private static final DataSource getMySqlDataSource() {
+		return null;
+	}
+	
+	/*DO NOT USE THIS IN PROD - METHOD JUST FOR OCP PREP*/
+	private static final Connection getConnection() throws SQLException{
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);	
+		return conn;
+	}
+	
+	private static PreparedStatement prepareStatement(String operation, Connection conn, String... paramsSQL) throws SQLException {
+		PreparedStatement stmnt = null;
+		if(paramsSQL.length<1) {
+			return stmnt;
+		}else {
+			switch(operation) {
+				case "getUserbyUsername":
+					String sql = "SELECT * FROM wltmngr.users WHERE username = ?";
+					stmnt = conn.prepareStatement(sql);
+					stmnt.setString(1, paramsSQL[0]);
+					break;
+			}
+			return stmnt;
+		}
 	}
 
 }
--- src/ro/astl/userservice/ws/UserInstanceImpl.java
+++ src/ro/astl/userservice/ws/UserInstanceImpl.java
@@ -2,20 +2,28 @@ package ro.astl.userservice.ws;
 
 import javax.jws.WebService;
 
+import ro.astl.userservice.dao.DaoLayer;
+import ro.astl.userservice.dao.DaoLayerImpl;
+import ro.astl.userservice.model.User;
 import ro.astl.userservice.request.UserInstanceIn;
 import ro.astl.userservice.response.UserInstanceOut;
 @WebService(endpointInterface="ro.astl.userservice.ws.UserInstance",
 			serviceName="UserInstanceService")
 public class UserInstanceImpl implements UserInstance {
+	DaoLayer dao = DaoLayerImpl.getInstance();
 
 	@Override
 	public UserInstanceOut getUserbyUsername(UserInstanceIn request) {
 		UserInstanceOut output = new UserInstanceOut();
-		output.setResponseCode("SUCCESS");
-		output.setId(1);
-		output.setUsername("andrei");
-		output.setPassword("test1234");
-		
+		User userResponse = dao.getUserbyUsername(request.getUsername());
+		if(userResponse.getUsername()!=null) {
+			output.setResponseCode("SUCCESS");
+			output.setId(userResponse.getId());
+			output.setUsername(userResponse.getUsername());
+			output.setPassword(userResponse.getPassword());
+		}else {
+			output.setResponseCode("NODATA");
+		}
 		return output;
 	}
 
